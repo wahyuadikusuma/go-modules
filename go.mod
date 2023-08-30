88bb8d0475b45bcea31adc583ca5e4bb612c8e01
Wahyu Adi Kusuma <wahyuadikusuma@gmail.com>
2023-08-30 12:45:14 +0700

    try to major update, update modules to v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/wahyuadikusuma/go-modules
+module github.com/wahyuadikusuma/go-modules/v2
 
 go 1.20
--- say_helo.go
+++ say_helo.go
@@ -1,5 +1,5 @@
 package say_hello
 
-func SayHello(nama string) string {
-	return "Halo Brader, " + nama + "!"
+func SayHello(nama string, kota string) string {
+	return "Halo Brader, nama saya " + nama + ". Saya berasal dari " + kota + "."
 }
